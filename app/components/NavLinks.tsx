d57d76cb2fc8650060e465332c16e0cc58c05673
Robert Dominic <devrobert.codes@gmail.com>
2026-01-26 19:01:14 +0000

    refactor: allow optional click handling in NavLink component

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link"
 import type { NavLinkProps } from "@/app/types"
 
-export default function NavLink({ href, children, isActive }: NavLinkProps) {
+export default function NavLink({ 
+    href, 
+    children, 
+    isActive,
+    onClick,
+   }: NavLinkProps) {
   return (
     <li className="text-[0.85rem] uppercase">
       <Link
         href={href}
+        onClick={onClick}
         className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-primary ${isActive ? "text-primary" : "text-gray-600"}`}
       >{children}</Link>
     </li>
